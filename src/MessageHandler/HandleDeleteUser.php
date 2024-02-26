135e9f2b8404cffa2a1c39eacf6fdd4244ff683c
ruhulfbr <ruhul11bd@gmail.com>
2024-02-26 15:05:46 +0600

    Supervisor uses

--- src/MessageHandler/HandleDeleteUser.php
+++ src/MessageHandler/HandleDeleteUser.php
@@ -5,15 +5,22 @@
 namespace App\MessageHandler;
 
 use App\Message\DeleteUser;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 class HandleDeleteUser
 {
+    public function __construct(private readonly LoggerInterface $logger)
+    {
+
+    }
+
     public function __invoke(DeleteUser $message): void
     {
         $userId = $message->getUserId();
         print_r("Delete User Request for user ID = " . $userId . PHP_EOL);
+        $this->logger->info("Delete User Request for user ID = " . $userId . PHP_EOL);
 
     }
 }
--- src/MessageHandler/HandleSendWelcomeMail.php
+++ src/MessageHandler/HandleSendWelcomeMail.php
@@ -5,16 +5,20 @@
 namespace App\MessageHandler;
 
 use App\Message\SendWelcomeEmail;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 class HandleSendWelcomeMail
 {
+    public function __construct(private readonly LoggerInterface $logger)
+    {
+
+    }
+
     public function __invoke(SendWelcomeEmail $message): void
     {
         $userJsonData = $message->getContent();
-        print_r("Got message = " . $userJsonData . PHP_EOL);
-
         $userData = json_decode($userJsonData);
 
         $subject = "Welcome to JOB Queue World";
@@ -23,17 +27,20 @@ class HandleSendWelcomeMail
 
         $retryCount = 0;
         while ($retryCount < 3) {
-//            $sent = mail($to, $subject, $msg);
+            // $sent = mail($to, $subject, $msg);
             $sent = rand(0, 1);
 
             if ($sent) {
                 print_r("Email Successfully Sent." . PHP_EOL);
+                $this->logger->info("Email Successfully Sent." . PHP_EOL);
                 return;
+                
             } else {
                 $retryCount++;
             }
         }
 
+        $this->logger->error("Send Welcome Email: Failed to send email after 3 retries." . PHP_EOL);
         print_r("Failed to send email after 3 retries." . PHP_EOL);
     }
 }
